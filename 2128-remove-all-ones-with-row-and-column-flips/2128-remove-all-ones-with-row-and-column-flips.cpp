e35b0e47275fa324604652d771b49cd676a0d669
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-09-06 20:12:44 +0530

    Time: 43 ms (54.05%), Space: 24.5 MB (24.32%) - LeetHub

---
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool removeOnes(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if(m == 0) return true;
+        if(m == 1 && n == 1) return true;
+        
+        //traverse first row and check for 1s if 1 found then reverse tat column
+        for(int c = 0; c < n; c++) {
+            if(grid[0][c] == 1) { 
+                for(int r = 0; r < m; r++) {
+                    grid[r][c] = 1 - grid[r][c];
+                }
+            }
+        }
+        
+        for(int r = 1; r < m; r++) {
+            int sum = 0;
+            for(int c = 0; c < n; c++) {
+                sum += grid[r][c];
+            }
+            if(sum != 0 && sum < n) return false;
+        }
+        
+        return true;
+        
+    }
+};
