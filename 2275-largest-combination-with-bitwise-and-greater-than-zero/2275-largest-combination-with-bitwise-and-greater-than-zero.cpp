d697d1f8a98ee902207235d6136c776782352d17
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-09-12 03:44:51 +0530

    Time: 95 ms (94.29%), Space: 57.5 MB (71.09%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -3,6 +3,8 @@ public:
    
     int largestCombination(vector<int>& cand) {
         int maxi=0;
+        //check iff the ith bit of each number is set, and find the total count of ith bit for each number
+        //the highest count of set bits among all numbers is the answer as it ll generate a AND value of more than 0  -- debraj notes
         for(int i=0;i<32;i++){
             int c=0;
             for(int x : cand) 
