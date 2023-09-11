3062080811ce4a22aa9af44b2cd59b3c2adda505
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-09-11 11:03:44 +0530

    Time: 11 ms (57.70%), Space: 12.5 MB (88.22%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,30 +2,17 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         map<int, vector<int> > mp;
+        vector<vector<int>> res;
+        
         for(int i = 0; i < groupSizes.size(); i++) {
             mp[groupSizes[i]].push_back(i);
+            if(mp[groupSizes[i]].size()== groupSizes[i]) {
+                res.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]].clear();
+            }
         }
-        vector<vector<int>> res;
-        for(auto it: mp) {
-            int curr = 0;
-            int targetSize = it.first;
-            vector<int> temp;
-            vector<int> people = it.second;
-            
-            for(int i = 0; i < people.size(); i++) {
-                int p = people[i];
-                if(p == -1)  
-                    continue;
-                temp.push_back(p);
-                curr++;
-                people[i] = -1;
-                if(curr == targetSize) {
-                    res.push_back(temp);
-                    temp = {};
-                    curr = 0;
-                }
-            }            
-        }
+        
         return res;
+        
     }
 };
