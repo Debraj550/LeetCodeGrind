a7b63581c48b201710f40f2178b7c9c6d4465396
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-09-15 02:27:41 +0530

    Time: 92 ms (78.53%), Space: 71.7 MB (20.62%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -4,7 +4,7 @@ public:
         long long ans = 0, sum = 0;
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
-            long long avg = (sum+i)/(i+1);
+            long long avg = ceil((double)(sum)/(double)(i+1));
             ans = max(ans, avg);
         }
         return ans;
