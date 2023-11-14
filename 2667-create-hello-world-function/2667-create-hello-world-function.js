5a44d65bd592e13df9872674ca58fff3260f18c5
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-11-15 03:43:16 +0530

    Time: 53 ms (33.70%), Space: 41.3 MB (89.72%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    const f = () => {
+        return "Hello World";
+    }
+    return f;
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
