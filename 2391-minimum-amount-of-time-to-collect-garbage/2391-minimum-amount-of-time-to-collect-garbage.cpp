23ced13560ced53fcd2859bc423d867185d7d5eb
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-11-21 05:27:43 +0530

    Time: 198 ms (13.10%), Space: 102.4 MB (91.88%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        for (int i = 1; i < travel.size(); i++) {
+            travel[i] = travel[i - 1] + travel[i];
+        }
+        
+        unordered_map<char, int> garbageLastPos;
+        int ans = 0;
+        
+        for (int i = 0; i < garbage.size(); i++) {
+            for (char c : garbage[i]) {
+                garbageLastPos[c] = i;
+            }
+            ans +=  garbage[i].size();
+        }
+        
+        string garbageTypes = "MPG";
+        for (char c : garbageTypes) {
+            ans += (garbageLastPos[c] == 0 ? 0 : travel[garbageLastPos[c] - 1]);
+        }
+        
+        return ans;
+    }
+};
