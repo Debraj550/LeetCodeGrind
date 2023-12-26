4fb89f26a871db3319e7f8bb892527580bf11e3d
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2023-12-27 02:23:46 +0530

    Time: 39 ms (25.27%), Space: 8.8 MB (65.22%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -3,20 +3,20 @@ public:
     const int MOD = 1e9 + 7;
     
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> memo(n + 1, vector<int>(target + 1, 0));
-        memo[n][target] = 1;
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        dp[n][target] = 1;
         
         for (int diceIndex = n - 1; diceIndex >= 0; diceIndex--) {
             for (int currSum = 0; currSum <= target; currSum++) {
                 int ways = 0;
                 for (int i = 1; i <= min(k, target - currSum); i++) {
-                    ways = (ways + memo[diceIndex + 1][currSum + i]) % MOD;
+                    ways = (ways + dp[diceIndex + 1][currSum + i]) % MOD;
                 }
                 
-                memo[diceIndex][currSum] = ways;
+                dp[diceIndex][currSum] = ways;
             }
         }
         
-        return memo[0][0];
+        return dp[0][0];
     }
 };
