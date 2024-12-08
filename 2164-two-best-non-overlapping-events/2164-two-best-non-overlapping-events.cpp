c193a98fb2e5278259a5a26b77d85d046e0c64ea
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2024-12-08 20:41:44 +0530

    Time: 162 ms (26.12%), Space: 155.2 MB (23.93%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
+    int upperBound(vector<vector<int>>& events, int idx){
+        int l = idx + 1, h = events.size() - 1, nextIndex = events.size();
+        while (l <= h) {
+            int mid = (l+h)/2;
+            if (events[mid][0] > events[idx][1]){
+                nextIndex = mid;
+                h = mid - 1;
+            }else{
+               l = mid + 1; 
+            }
+        }
+        return nextIndex;
+    }
+    int solve(vector<vector<int>> &events, int idx, int jobs, vector<vector<int>> &dp) {
+        if(jobs == 2 || idx == events.size()) return 0;
+        if(dp[idx][jobs] != -1) return dp[idx][jobs];
+        int nextindex = upperBound(events, idx);
+        int include = events[idx][2] + solve(events, nextindex, jobs + 1, dp);
+        int notInclude = solve(events, idx + 1, jobs, dp);
+        return dp[idx][jobs] = max(include, notInclude);
+    }
     int maxTwoEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end());
-        int maxValue = 0;
-        int res = 0;
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int,pair<int,int>>> > pq;
-        
-        for(int i = 0; i < events.size(); i++) {
-            while(!pq.empty() && pq.top().first < events[i][0] ) {
-                maxValue = max(maxValue, pq.top().second.second);
-                pq.pop();
-            }
-
-            pq.push({events[i][1], {events[i][0], events[i][2]}});
-            res = max(res, maxValue + events[i][2]);
-        }
-
-        return res;
+        int n = events.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solve(events, 0, 0, dp);
     }
 };
+
+
