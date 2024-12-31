94c01854043068cd704186f362b11e25d63173be
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2024-12-31 15:07:19 +0530

    Time: 41 ms (5.07%), Space: 16.2 MB (8.32%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int> &days, vector<int> &costs, int dayIdx, int travelledTill, vector<vector<int>> &dp) {
+    unordered_map<string, int> dp;
+    int solve(vector<int> &days, vector<int> &costs, int dayIdx, int travelledTill) {
         if(dayIdx >= days.size()) return 0;
-        if(dp[dayIdx][travelledTill] != -1) return dp[dayIdx][travelledTill];
-        if(travelledTill > days[dayIdx]) return solve(days, costs, dayIdx + 1, travelledTill, dp);
+        string cacheKey = to_string(dayIdx)+"-"+to_string(travelledTill);
+        if(dp.find(cacheKey) != dp.end()) return dp[cacheKey];
+        if(travelledTill > days[dayIdx]) return solve(days, costs, dayIdx + 1, travelledTill);
 
-        int oneDayPassCost = costs[0] + solve(days, costs, dayIdx + 1, days[dayIdx] + 1, dp);
-        int sevenDayPassCost = costs[1] + solve(days, costs, dayIdx + 1, days[dayIdx] + 7, dp);
-        int ThirtyDayPassCost = costs[2] + solve(days, costs, dayIdx + 1, days[dayIdx] + 30, dp);
+        int oneDayPassCost = costs[0] + solve(days, costs, dayIdx + 1, days[dayIdx] + 1);
+        int sevenDayPassCost = costs[1] + solve(days, costs, dayIdx + 1, days[dayIdx] + 7);
+        int ThirtyDayPassCost = costs[2] + solve(days, costs, dayIdx + 1, days[dayIdx] + 30);
 
-        return dp[dayIdx][travelledTill] = min(oneDayPassCost, min(sevenDayPassCost, ThirtyDayPassCost));
+        return dp[cacheKey] = min(oneDayPassCost, min(sevenDayPassCost, ThirtyDayPassCost));
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int ans = 0;
-        vector<vector<int>> dp(days.size(), vector<int>(1000, -1));
-        ans = solve(days, costs, 0, 0, dp);
+        ans = solve(days, costs, 0, 0);
         return ans;
     }
 };
+
