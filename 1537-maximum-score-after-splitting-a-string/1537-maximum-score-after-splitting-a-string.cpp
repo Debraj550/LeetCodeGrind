1321a2cd3ca2cd057a3386865f542e47a92bb7fa
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-02 04:35:43 +0530

    Time: 3 ms (18.59%), Space: 6.7 MB (100%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.size();
-        vector<int> zeroCount(n, 0), oneCount(n, 0);
-        zeroCount[0] = (s[0] == '0');
-        for (int i = 1; i < n; i++) {
-            zeroCount[i] = zeroCount[i - 1] + (s[i] == '0');
+        int ones = count(s.begin(), s.end(), '1');
+        
+        int ans = 0;
+        int zeros = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+            
+            ans = max(ans, zeros + ones);
         }
-
-        oneCount[n - 1] = (s[n - 1] == '1');
-        for (int i = n - 2; i >= 0; i--) {
-            oneCount[i] = oneCount[i + 1] + (s[i] == '1');
-        }
-        int maxScore = INT_MIN;
-        for (int i = 1; i < n; i++) {
-            int score = zeroCount[i - 1] + oneCount[i];
-            maxScore = max(maxScore, score);
-        }
-
-        return maxScore;
+        
+        return ans;
     }
 };
