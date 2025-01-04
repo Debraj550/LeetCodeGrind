06f669c68db8fc307416047b141f73858d52db4a
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-05 00:15:20 +0530

    Time: 23 ms (4.47%), Space: 18.8 MB (7.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     unordered_map<string, int> cache;
-    
     int solve(vector<vector<int>> &triangle, int m, int row, int col) {
-        if (row >= m) return 0;
+        if(row >= m) return 0;
+        if(col >= triangle[row].size()) return 0;
         string cacheKey = to_string(row) + "-" + to_string(col);
-        if (cache.find(cacheKey) != cache.end()) return cache[cacheKey];
+        if(cache.find(cacheKey) != cache.end()) return cache[cacheKey];
+
         int currVal = triangle[row][col];
-        int result = currVal + min(
-            solve(triangle, m, row + 1, col),
-            solve(triangle, m, row + 1, col + 1)
-        );
-        return cache[cacheKey] = result;
+        return cache[cacheKey] = currVal + min(solve(triangle, m, row+1, col), solve(triangle, m, row+1, col+1));
     }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         return solve(triangle, triangle.size(), 0, 0);
     }
