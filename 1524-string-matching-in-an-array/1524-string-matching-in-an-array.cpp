20ecb22d3c61838972ed628874de10d63516cbb3
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-08 04:56:53 +0530

    Time: 11 ms (12.17%), Space: 15.5 MB (8.99%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,17 +1,64 @@
 class Solution {
 public:
+    vector<int> computeLPS(const string& pat) {
+        int n = pat.length();
+        vector<int> lps(n, 0);
+        int len = 0;
+
+        for(int i = 1; i < n; ++i){
+            while(len > 0 && pat[i] != pat[len]){
+                len = lps[len -1];
+            }
+            if(pat[i] == pat[len]){
+                len++;
+                lps[i] = len;
+            }
+        }
+        return lps;
+    }
+
+    bool isPatternInText(const string& txt, const string& pat) {
+        if(pat.empty()) return true;
+        if(txt.length() < pat.length()) return false;
+
+        vector<int> lps = computeLPS(pat);
+        int i = 0;
+        int j = 0;
+
+        while(i < txt.length()){
+            if(txt[i] == pat[j]){
+                i++;
+                j++;
+                if(j == pat.length()){
+                    return true;
+                }
+            }
+            else{
+                if(j != 0){
+                    j = lps[j -1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
     vector<string> stringMatching(vector<string>& words) {
-        int n = words.size();
-        vector<string> ans;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i != j && words[j].find(words[i]) != string::npos) {
-                    ans.push_back(words[i]);
+        vector<string> res;
+        sort(words.begin(), words.end(), [](const string& a, const string& b) -> bool {
+            return a.length() < b.length();
+        });
+
+        for(int i = 0; i < words.size(); ++i){
+            for(int j = i + 1; j < words.size(); ++j){
+                if(isPatternInText(words[j], words[i])){
+                    res.push_back(words[i]);
                     break;
                 }
             }
         }
-
-        return ans;
+        return res;
     }
 };
