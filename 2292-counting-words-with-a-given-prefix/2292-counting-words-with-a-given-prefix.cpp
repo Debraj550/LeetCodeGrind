f24856aea46513f40091c5e06785bc0f560769fc
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-10 03:14:26 +0530

    Time: 69 ms (5.09%), Space: 63.7 MB (5%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,11 +1,49 @@
+class TrieNode {
+public:
+    unordered_map<char, TrieNode *> children;
+    int count;
+    TrieNode() : count(0) {}
+};
+
+class Trie {
+private: 
+    TrieNode *root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode *curr = root;
+        for(char c: word) {
+            if(!curr->children[c]) {
+                curr->children[c] = new TrieNode();
+            }
+            curr = curr->children[c];
+            curr->count++;
+        }
+    }
+
+    int count(string pref) {
+        TrieNode *curr = root;
+        for(char c: pref) {
+            if(!curr->children[c]) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return curr->count;
+    }
+};
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int res = 0;
+        Trie trie;
         for(auto word: words) {
-            int startingIdx = word.find(pref);
-            if(startingIdx == 0) res++;
+            trie.insert(word);
         }
-        return res;
+        return trie.count(pref);
     }
 };
