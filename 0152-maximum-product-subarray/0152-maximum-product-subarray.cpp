d7e03685aaf9e94c1dcbbe0f36fb865c0a697fe9
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-10 03:49:14 +0530

    Time: 55 ms (10.43%), Space: 22.4 MB (15.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
+    int solve(vector<int> &nums, int idx, int maxi, int mini) {
+        if(idx >= nums.size()) return maxi;
+
+        int curr = nums[idx];
+
+        int maxTillNow = max({curr, curr * maxi, curr * mini});
+        int minTillNow = min({curr, curr * maxi, curr * mini});
+
+        return max(maxTillNow, solve(nums, idx + 1, maxTillNow, minTillNow));
+    }
     int maxProduct(vector<int>& nums) {
-        int curr = nums[0];
-        int maxi = nums[0];
-        int mini = nums[0];
+        // int curr = nums[0];
+        // int maxi = nums[0];
+        // int mini = nums[0];
         
-        for(int i = 1; i < nums.size(); i++) {
-            int prod = max(nums[i], max(nums[i] * maxi, nums[i] * mini));
-            mini = min(nums[i], min(nums[i] * maxi, nums[i] * mini));
-            maxi = prod;
-            curr = max(maxi, curr);
-        }
-        return curr;
+        // for(int i = 1; i < nums.size(); i++) {
+        //     int prod = max(nums[i], max(nums[i] * maxi, nums[i] * mini));
+        //     mini = min(nums[i], min(nums[i] * maxi, nums[i] * mini));
+        //     maxi = prod;
+        //     curr = max(maxi, curr);
+        // }
+        // return curr;
+
+        return solve(nums, 0, 1, 1);
     }
 };
