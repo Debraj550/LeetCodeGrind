4e1334e3a43ed7d6d4c9c31c15b995d0a5144e0e
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-13 15:59:25 +0530

    Time: 3 ms (31.21%), Space: 15.6 MB (6.73%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        int minFallingSum = INT_MAX;
+        vector<vector<optional<int>>> memo(
+            matrix.size(), vector<optional<int>>(matrix.size(), nullopt));
 
-        for(int i = m - 1; i >= 0; i--) {
-            for(int j = 0; j < n; j++) {
-                if(i == m-1) dp[i][j] = matrix[i][j];
-                else {
-                    int bottomLeft = j > 0 ? dp[i+1][j-1] : INT_MAX; 
-                    int bottom = dp[i+1][j];
-                    int bottomRight = j < n-1 ? dp[i+1][j+1]: INT_MAX;
-                    dp[i][j] = matrix[i][j] + min({bottomLeft, bottom, bottomRight});
-                }
-            }
+        // start a DFS (with memoization) from each cell in the top row
+        for (int startCol = 0; startCol < matrix.size(); startCol++) {
+            minFallingSum = min(minFallingSum, findMinFallingPathSum(
+                                                   matrix, 0, startCol, memo));
+        }
+        return minFallingSum;
+    }
+
+    int findMinFallingPathSum(vector<vector<int>>& matrix, int row, int col,
+                              vector<vector<optional<int>>>& memo) {
+        // base cases
+        if (col < 0 || col == matrix.size()) {
+            return INT_MAX;
+        }
+        // check if we have reached the last row
+        if (row == matrix.size() - 1) {
+            return matrix[row][col];
+        }
+        // check if the results are calculated before
+        if (memo[row][col] != nullopt) {
+            return (memo[row][col]).value_or(0);
         }
 
-        int res = INT_MAX;
-        for(int i = 0; i < n; i++) {
-            res = min(res, dp[0][i]);
-        }
-        return res;
+        // calculate the minimum falling path sum starting from each possible
+        // next step
+        int left = findMinFallingPathSum(matrix, row + 1, col - 1, memo);
+        int middle = findMinFallingPathSum(matrix, row + 1, col, memo);
+        int right = findMinFallingPathSum(matrix, row + 1, col + 1, memo);
+
+        int minSum = min(left, min(middle, right)) + matrix[row][col];
+
+        memo[row][col] = minSum;
+        return minSum;
     }
 };
