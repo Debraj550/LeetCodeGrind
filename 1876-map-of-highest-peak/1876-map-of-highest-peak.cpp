1762fa31bd4ef5934dc87f9d62fda4f1f676d21b
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2025-01-22 16:07:06 +0530

    Time: 1064 ms (5.09%), Space: 346.2 MB (5.09%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,53 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> dirs = {{0,1}, {1,0}, {0,-1},{-1,0}};
+
+    bool isValid(int m, int n, int i, int j) {
+        return (i >= 0 && i < m && j >= 0 && j < n) ? true: false;
+    }
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         int m = isWater.size(), n = isWater[0].size();
-        int maxi = (m * n);
-        vector<vector<int>> heights(m, vector<int>(n, maxi));
-
+        
+        vector<vector<int>> heights(m, vector<int>(n, -1));
+        queue<pair<int,int>> q;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(isWater[i][j]) heights[i][j] = 0; 
+                if(isWater[i][j]) {
+                    heights[i][j] = 0;
+                    q.push({i,j});
+                }
             }
         }
 
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                int dist = maxi;
-                int ni = i - 1;
-                int nc = j;
-                if(ni >= 0 && ni < m && nc >= 0 && nc < n ) {
-                    dist = min(dist, heights[ni][nc]);
+        int nextHeight = 1;
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                auto currCell = q.front();
+                q.pop();
+                for(auto dir: dirs) {
+                    int nr = currCell.first + dir[0];
+                    int nc = currCell.second + dir[1];
+                    if(isValid(m, n, nr, nc) && heights[nr][nc] == -1) {
+                        heights[nr][nc] = nextHeight;
+                        q.push({nr, nc});
+                    }
                 }
-
-                ni = i;
-                nc = j - 1;
-                if(ni >= 0 && ni < m && nc >= 0 && nc < n ) {
-                    dist = min(dist, heights[ni][nc]);
-                }
-
-                heights[i][j] = min(heights[i][j], dist + 1);
             }
-        }
-
-        for(int i = m - 1; i >= 0; i--) {
-            for(int j = n - 1; j >= 0; j--) {
-                int dist = maxi;
-                int ni = i + 1;
-                int nj = j;
-                if(ni >= 0 && ni < m && nj >= 0 && nj < n ) {
-                    dist = min(dist, heights[ni][nj]);
-                }
-                ni = i;
-                nj = j + 1;
-                if(ni >= 0 && ni < m && nj >= 0 && nj < n ) {
-                    dist = min(dist, heights[ni][nj]);
-                }
-
-                heights[i][j] = min(heights[i][j], dist + 1);
-            }
+            nextHeight++;
         }
 
         return heights;
