f14e5509926dfcaf283f80e4d1a0bd9b9da7c7b0
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2026-01-05 03:22:16 +0530

    Time: 4 ms (75.24%), Space: 57 MB (90.48%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var appendCharacters = function(s, t) {
+    let i = 0, j = 0;
+        while(i < s.length && j < t.length) {
+            if(s[i] === t[j]) {
+                j++;
+            }
+            i++;
+        }
+
+        if(j === t.length) return 0;
+        return t.length - j;
+};
