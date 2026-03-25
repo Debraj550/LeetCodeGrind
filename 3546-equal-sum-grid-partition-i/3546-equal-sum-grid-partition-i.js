980237f071d7ba03ae83185745c0cecbd854406e
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2026-03-26 01:38:03 +0530

    Time: 44 ms (46.15%), Space: 85.8 MB (38.46%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var canPartitionGrid = function(grid) {
+    let m = grid.length;
+    let n = grid[0].length;
+
+    let totalSum = 0;
+
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) totalSum += grid[i][j];
+    }
+    if(totalSum % 2 !== 0) return false;
+
+    let target = totalSum / 2;
+
+    // horizontal cut
+    let rowSum = 0;
+    for(let i = 0; i < m-1; i++) {
+        for(let j = 0; j < n; j++) rowSum += grid[i][j];
+        if(rowSum === target) 
+            return true;
+    }
+
+    // vertical cut
+    let colSums = new Array(n).fill(0);
+    for(let j = 0; j < n; j++) {
+        for(let i = 0; i < m; i++) 
+            colSums[j] += grid[i][j];
+    }
+
+    let colSum = 0;
+    for(let j = 0; j < n; j++) {
+        colSum += colSums[j];
+        if(colSum === target) return true;
+    }
+    return false;
+};
+
+
