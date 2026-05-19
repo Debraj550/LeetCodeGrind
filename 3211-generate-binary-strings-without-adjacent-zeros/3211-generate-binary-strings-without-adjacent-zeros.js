35856722bb83dc2a40717a88881c97efa92e0912
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2026-05-19 19:01:41 +0530

    Time: 53 ms (77.14%), Space: 58.3 MB (74.29%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var validStrings = function(n) {
+    let res = [];
+    function solve(index, curr) {
+        if(index === n) {
+            res.push(curr);
+            return;
+        }
+        if(curr.length > 0 && curr[index-1] === '0') solve(index+1, curr + '1');
+        else {
+            solve(index + 1, curr + '0');
+            solve(index + 1, curr + '1');
+        }
+    }
+    solve(0, "");
+    return res;
+};
