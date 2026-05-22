ee61f18026b8b48308aa9a709742a496d6b2edbe
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2026-05-22 19:32:10 +0530

    Time: 2 ms (94.93%), Space: 63.5 MB (77.9%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -1,25 +1,20 @@
-/**
- * @param {number[][]} graph
- * @return {number[][]}
- */
-function findPossible(curr, end, graph, temp, res) {
-    if(curr === end) {
-        res.push([...temp, curr]);
-        return;
-    }
-   
-    let canMove = graph[curr];
-    for(let i = 0; i < canMove.length; i++) {
-        let toNode = canMove[i];
-        temp.push(curr);
-        findPossible(toNode, end, graph, temp, res);
-        temp.pop();
-    }
-    return;
-}
+
 var allPathsSourceTarget = function(graph) {
-    let nodes = graph.length;
     let res = [];
-    findPossible(0, nodes - 1, graph, [], res);
+    let path = [];
+
+    function dfs(node) {
+        path.push(node);
+        if (node === graph.length - 1) {
+            res.push([...path]);
+        } else {
+            for (let next of graph[node]) {
+                dfs(next);
+            }
+        }
+        path.pop();
+    }
+
+    dfs(0);
     return res;
 };
