57794124409eb0a23249ff8d72a58fc2f8b6e1ec
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2026-07-07 01:29:20 +0530

    Time: 39 ms (56.61%), Space: 80.4 MB (71.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,20 +3,16 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length === 0) return 0;
-    const mp = new Map();
-    for(let num of nums) mp.set(num, true);
-    let longest = -Infinity;
-    for(let [key, val] of mp) {
-        if(!mp.has(key-1)) {
-            let currStreak = 1;
-            let currVal = key
-            while(mp.has(currVal+1)) {
-                currVal += 1;
-                currStreak++;
-            }
-            longest = Math.max(longest, currStreak);
+    const set = new Set(nums);
+    let longest = 0;
+
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            let end = num;
+            while (set.has(end + 1)) end++;
+            longest = Math.max(longest, end - num + 1);
         }
     }
+
     return longest;
 };
