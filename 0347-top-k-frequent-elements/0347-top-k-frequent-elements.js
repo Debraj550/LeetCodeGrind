1ddfa16b4c5c6323286d8f787d2245a25ef7f3ed
Debraj Dhar <32432161+Debraj550@users.noreply.github.com>
2026-07-08 23:13:30 +0530

    Time: 19 ms (29.68%), Space: 63.9 MB (11%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const mp = new Map();
+    for(let num of nums)
+        mp.set(num, (mp.get(num) || 0) + 1);
+    
+    const bucket = Array(nums.length + 1).fill().map(() => []);
+    for(const [num, count] of mp) {
+        bucket[count].push(num);
+    }
+    const res = [];
+    for(let i = bucket.length-1; i >= 0 && res.length < k; i--){
+        if(bucket[i].length)
+            res.push(...bucket[i]);
+    }
+    return res;
+};
